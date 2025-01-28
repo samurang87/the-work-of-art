d70896f7316257fe231d8e097b3a8b40d9ccb0cc
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-01-28 15:02:30 +0100

    feat: Add Feed page with placeholder text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css'
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
+import {Route, Routes} from "react-router-dom";
+import Feed from "./pages/Feed.tsx";
 
 function App() {
     return (
         <div className="min-h-screen bg-gray-50">
             <Header />
             <main className="container mx-auto px-4 py-8 mt-16">
-                <h1>Henlo world</h1>
+                <Routes>
+                    <Route path={"/"} element={<Feed/>} />
+                    <Route path={"/feed"} element={<Feed/>} />
+                </Routes>
             </main>
             <Footer />
         </div>
