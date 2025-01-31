705c1b1cce590a2584c82cf7388f31c4f5ef42a4
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-01-31 14:43:59 +0100

    fix: Use RestController instead of Controller

--- backend/src/main/kotlin/de/neuefische/backend/user/UserController.kt
+++ backend/src/main/kotlin/de/neuefische/backend/user/UserController.kt
@@ -1,12 +1,12 @@
 package de.neuefische.backend.user
 
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping("api/user")
 class UserController(private val userService: UserService) {
 
