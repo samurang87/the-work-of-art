b73853cf1766727df656eb12982dfdd32d1a0fe1
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-01-31 17:57:50 +0100

    chore: Add missing slash to request mapping
    Doesn't change anything but bothers me

--- backend/src/main/kotlin/de/neuefische/backend/user/UserController.kt
+++ backend/src/main/kotlin/de/neuefische/backend/user/UserController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("api/user")
+@RequestMapping("/api/user")
 class UserController(private val userService: UserService) {
 
     @GetMapping("/{name}")
