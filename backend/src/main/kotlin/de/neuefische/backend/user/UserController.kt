3ba2eafdff0c06b6322836e7f20098804fd95a9e
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-02-04 16:52:28 +0100

    chore: Replace wildcard imports with specific ones
    Looks like it is not a good practice in Kotlin to use wildcard imports

--- backend/src/main/kotlin/de/neuefische/backend/user/UserController.kt
+++ backend/src/main/kotlin/de/neuefische/backend/user/UserController.kt
@@ -1,7 +1,10 @@
 package de.neuefische.backend.user
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/user")
