835201a36a531d303a0726523f46208b2e600141
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-02-04 15:18:43 +0100

    feat: Fetch all works of art in the feed page

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -1,31 +1,41 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
+import WorkOfArtComponent from "../components/WorkOfArtComponent";
+import {WorkOfArt} from "../types.tsx";
 
 export default function Feed() {
-    const [status, setStatus] = useState<string>('');
+    const [worksOfArt, setWorksOfArt] = useState<WorkOfArt[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-        async function fetchStatus() {
+        async function fetchWorksOfArt() {
             try {
-                const response = await axios.get<string>('/api/status');
-                setStatus(response.data);
+                const response = await axios.get<WorkOfArt[]>('/api/woa');
+                setWorksOfArt(response.data);
+                setError(null);
             } catch (error) {
                 setError(error instanceof Error ? error.message : 'An error occurred');
             } finally {
                 setLoading(false);
             }
         }
-        void fetchStatus();
+
+        void fetchWorksOfArt();
     }, []);
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
+    if (loading) return <div className="mt-24 text-center">Loading...</div>;
+    if (error) return <div
+        className="mt-24 text-center text-red-500">Error: {error}</div>;
 
     return (
-        <div>
-            <p>The status of the backend is: {status}</p>
+        <div className="container mx-auto px-4 mt-24 pb-24">
+            <div className="max-w-2xl mx-auto space-y-6">
+                {worksOfArt.map(workOfArt => (
+                    <WorkOfArtComponent key={workOfArt.id} workOfArt={workOfArt}
+                                        allFields={false}/>
+                ))}
+            </div>
         </div>
     );
 }
