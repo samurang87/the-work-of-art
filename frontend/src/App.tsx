28dabd1358582e15c28794a9111ba9be820efc87
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-02-05 16:50:57 +0100

    feat: Add Logout in the navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Feed from "./pages/Feed.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
 import WorkOfArtPage from "./pages/WorkOfArtPage.tsx";
@@ -12,6 +12,7 @@ import axios from "axios";
 
 function App() {
   const [username, setUsername] = useState<string | undefined>();
+  const navigate = useNavigate();
 
   async function loadUser() {
     try {
@@ -22,13 +23,19 @@ function App() {
     }
   }
 
+  function logout() {
+    localStorage.removeItem("token");
+    setUsername(undefined);
+    void navigate("/");
+  }
+
   useEffect(() => {
     void loadUser();
   }, []);
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header />
+      <Header logout={logout} />
       <main className="container mx-auto px-4 py-8 mt-16">
         <Routes>
           <Route path="/" element={<LandingPage username={username} />} />
