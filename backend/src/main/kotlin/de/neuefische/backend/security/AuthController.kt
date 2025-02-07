9bea9cb3071640fabbc2616d396675d1091131f7
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-02-07 17:02:46 +0100

    feat: Add SecurityService to make security features available
    to controllers

--- backend/src/main/kotlin/de/neuefische/backend/security/AuthController.kt
+++ backend/src/main/kotlin/de/neuefische/backend/security/AuthController.kt
@@ -1,8 +1,6 @@
 package de.neuefische.backend.security
 
 import de.neuefische.backend.user.UserService
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,21 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/auth")
 class AuthController(
     private val userService: UserService,
+    private val securityService: SecurityService,
 ) {
     @GetMapping("/me")
-    fun getMe(
-        @AuthenticationPrincipal user: OAuth2User?,
-    ): String {
-        if (user == null) {
-            return ""
-        }
-
-        val oauthUsername: String = user.attributes["login"]?.toString() ?: ""
-
-        if (oauthUsername.isEmpty()) {
-            return ""
-        }
-
-        return userService.findOrCreateUser(oauthUsername).name
+    fun getMe(): String {
+        val username = securityService.getCurrentUsername() ?: return ""
+        return userService.findOrCreateUser(username).name
     }
 }
